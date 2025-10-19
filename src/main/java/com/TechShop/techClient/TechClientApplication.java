9c988d013f5bc3f076288f42970cf8d89d8d78ef
conductor0 <danielnordio6@gmail.com>
2025-10-19 20:31:53 -0300

    Inicialização do projeto

---
+++ src/main/java/com/TechShop/techClient/TechClientApplication.java
@@ -0,0 +1,13 @@
+package com.TechShop.techClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TechShop/techClient/TechClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TechShop.techClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
