0df281cced91c392ce5f2568cd711b8abff101a0
conductor0 <danielnordio6@gmail.com>
2025-10-21 22:01:32 -0300

    Funcionalidade de pesquisa de produtos adicionada

--- src/main/java/com/TechShop/techClient/controller/produtosController.java
+++ src/main/java/com/TechShop/techClient/controller/produtosController.java
@@ -4,10 +4,22 @@
  */
 package com.TechShop.techClient.controller;
 
-/**
- *
- * @author conductor
- */
+import com.TechShop.techClient.data.produtosEntity;
+import com.TechShop.techClient.service.ProdutosService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequestMapping("/produtos")
 public class produtosController {
+    @Autowired
+    ProdutosService produtosService;
     
+    @GetMapping("/listar")
+    public List listarProdutos(){
+        List <produtosEntity> produtos = produtosService.listarProdutos();
+        return produtos;
+    }
 }
